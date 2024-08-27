beb08f71dea34401b10b93d1d79b0d1d4e27e234
aliasghar.nasir <aliasghar.nasir@brightnexus.com>
2024-08-27 10:10:31 +0800

    Initial Commit

---
+++ CalculatorAli.py
@@ -0,0 +1,20 @@
+import CaclUi
+
+
+def main():
+
+    num1=int(input("Enter Num1: "))
+    num2=int(input("Enter Num2: "))
+
+    operation = 0
+    answer=0
+
+    operation=input("Choose operation to use Enter 1:Addition 2:Subtraction 3:Devide 4:Multiply: ")
+
+    CaclUi.CaclUi(operation,num1,num2)
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+        name = 'CalculatorAli',
+        version = '1.0',
+        py_modules = ['CalculatorAli'],
+        author = 'Muhammad Ali Asghar',
+        author_email = 'ali.asghar@brightnexus.com',
+        description = 'Performs basic calculation operations',
+        )
