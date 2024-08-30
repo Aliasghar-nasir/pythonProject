509637c80f7a0c218fe223d69b177d19906da3c1
aliasghar.nasir <aliasghar.nasir@brightnexus.com>
2024-08-30 16:25:07 +0800

    Update main code

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(
-        name = 'CalculatorAli',
-        version = '1.0',
-        py_modules = ['CalculatorAli'],
-        author = 'Muhammad Ali Asghar',
-        author_email = 'ali.asghar@brightnexus.com',
-        description = 'Performs basic calculation operations',
-        )
+    name='CalculatorAli',
+    version='1.0',
+    packages=find_packages(),
+    py_modules=['CalculatorAli'],
+    install_requires=[
+        'CaclUi',
+    ],
+        author='Muhammad Ali Asghar',
+        author_email='ali.asghar@brightnexus.com',
+        description='Performs basic calculation operations',
+        entry_points={
+                'console_scripts': [
+                        'CalculatorAli=pythonProject:CalculatorAli',  # Updated to reflect the new import path
+                ],
+        },
+)
