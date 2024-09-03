4cbdaf054b849150c879a45f28cc9a4b06d8e1a5
aliasghar.nasir <aliasghar.nasir@brightnexus.com>
2024-09-03 17:16:12 +0800

    Update main code

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='CalculatorAli',
-    version='2.0',
+    version='3.0',
     packages=find_packages(),
     py_modules=['CalculatorAli'],
     install_requires=[
